d4d7a9df027eecdbc9220c68dcb0284e005b99ca
진주현 <jinju0405@naver.com>
2022-02-08 16:58:53 +0900

    220208

---
+++ baek_01~06.py
@@ -0,0 +1,22 @@
+# 3
+print('\\    /\\')
+print(" )  ( ')")
+print('(  /  )')
+print(' \\(__)|')
+
+#4
+print('|\\_/|')
+print('|q p|   /}')
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
+
+#5
+a, b = input().split()
+sum = int(a) + int(b)
+print(sum)
+
+#6
+a, b = map(int, input().split())
+c = a-b
+print(c)
