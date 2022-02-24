66881496cf1c813d086cf78a1a066d4a059a5a79
진주현 <jinju0405@naver.com>
2022-02-24 21:15:18 +0900

    220224

---
+++ 2563_색종이.py
@@ -0,0 +1,17 @@
+N = int(input())
+arr = [[0]*100 for _ in range(100)]
+
+for n in range(N):
+    x, y = list(map(int, input().split()))
+
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            arr[i][j] = 1
+
+cnt = 0
+for i in range(100):
+    for j in range(100):
+        if arr[i][j] == 1:
+            cnt += 1
+
+print(cnt)
