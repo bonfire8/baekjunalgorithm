360a7a2a5b37dacb76311a17d4f5248fe4ff4b08
진주현 <jinju0405@naver.com>
2022-02-24 21:20:49 +0900

    220224

---
+++ 1316_그룹단어체커.py
@@ -0,0 +1,12 @@
+T =int(input())
+cnt = T
+for tc in range(T):
+    word = input()
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+
+print(cnt)
